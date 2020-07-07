f424b4e9358a2998210b98186cc2367d4f02e144
Kudikyala, Udai <udaikudikyala@gmail.com>
2020-07-07 09:40:09 +0530

    change in test code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+    @Test
+  public void greetShouldIncludeGreetingUdaiMessage() {
+    String someone = "World, Thanks you make Udai Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
